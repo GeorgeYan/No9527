cca3fa2ed92ab74c8c7d2e5beaf895d280e663e9
GeorgeYan <abcyys123@gmail.com>
2013-11-21 11:45:13 +0800

    first commit

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'http://ruby.taobao.org/'
+
+gem 'rails', '4.0.1'
+
+group :development, :test do
+  gem 'sqlite3', '~> 1.3.8'
+  gem 'rspec-rails', '~> 2.14.0'
+end
+
+group :test do
+  gem 'selenium-webdriver', '~> 2.37.0'
+  gem 'capybara', '~> 2.1.0'
+end
+
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails', '~> 3.0.4'
+gem 'turbolinks', '~> 1.3.1'
+gem 'jbuilder', '~> 1.5.2'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+group :production do
+  gem 'pg', '~> 0.17.0'
+  gem 'rails_12factor', '~> 0.0.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+No9527::Application.config.secret_key_base = 'ce82a2228a331f2c1027155ab6f3804a5d58f0a50f9bbf5ac8a8fb2299e3de3dac39fadbd756eaab96e8b46efdc1a3e1d9d2bd3d77f05910498f1c6c071f4874'
