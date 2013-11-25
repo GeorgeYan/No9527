d69f90599e802a116751d04ffe82768a7f988019
GeorgeYan <abcyys123@gmail.com>
2013-11-25 17:21:40 +0800

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,40 +1,17 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  subject { page }
 
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'No9527 App 哈哈'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('No9527 App 哈哈')
-    end
-
-    it "should have the title '主页'" do
-      visit '/static_pages/home'
-      expect(page).to have_title('主页')
-    end
-
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title('No9527')
-    end
-
-    it "should have hte content title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('|主页')
-    end
+    it { should have_title(full_title('')) }
+    it { should_not have_title('|主页') }
   end
 
   describe "Help page" do
-
-    it "应该包括内容'帮助'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('帮助')
-    end
-
-    it "should have the title '帮助'" do
-      visit '/static_pages/help'
-      expect(page).to have_title('帮助')
-    end
+    before { visit help_path }
+    it { should have_title(full_title('帮助')) }
   end
 end
---
+++ spec/support/utilities.rb
@@ -0,0 +1,8 @@
+def full_title(page_title)
+  base_title = "No9527"
+  if page_title.empty?
+    base_title
+  else
+    "#{base_title} | #{page_title}"
+  end
+end
