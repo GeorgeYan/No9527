74729fde0fd8f3b31b43411732aa1b787575ae2b
GeorgeYan <abcyys123@gmail.com>
2013-11-29 17:07:05 +0800

    set application.rb initialize_on_precompile

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module No9527
     # Config the default encoding used in templates for ruby 2.0
     config.encoding = 'utf-8'
 
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = 'zh-CN'
