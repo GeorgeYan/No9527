22e30d552775bbfd420c8a37ffdb4ac3b3016d5b
GeorgeYan <abcyys123@gmail.com>
2013-12-06 22:04:20 +0800

    ssh_options

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ set :user, "deployer"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
+set :ssh_options,{:forward_agent => true}
 
 set :scm, "git"
 set :repository, "git@github.com:GeorgeYan/#{application}.git"
